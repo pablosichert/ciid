70027a400489ec2ecfb02987d2adbe55c60250ef
Pablo Sichert <mail@pablosichert.com>
2019-10-15 01:29:39 +0200

    Improve error handling

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,6 @@ use chrono::DateTime;
 use clap::{App, Arg};
 use sha2::Digest;
 
-#[derive(Debug)]
-struct Error {
-    message: String,
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(formatter, "{}", self.message)
-    }
-}
-
-impl std::error::Error for Error {}
-
 unsafe fn transmute_vec<Input, Output>(
     data: Vec<Input>,
 ) -> Result<Vec<Output>, Box<dyn std::error::Error>> {
@@ -24,10 +11,10 @@ unsafe fn transmute_vec<Input, Output>(
     let factor = size_input / size_output;
 
     if size_output * factor != size_input {
-        return Err(Box::new(Error {
-            message: "Input size must be a multiple of output size".to_owned(),
-        }));
-    }
+        Err("Input size must be a multiple of output size")
+    } else {
+        Ok(())
+    }?;
 
     let pointer = data.as_ptr() as *mut Output;
     let length = data.len();
@@ -54,27 +41,44 @@ fn sortable_base_16(data: &[u8]) -> String {
     return result;
 }
 
-fn get_timestamp(file_path: &str) -> Result<[u8; 8], Box<dyn std::error::Error>> {
+fn exiftool(args: &[&str]) -> Result<String, Box<dyn std::error::Error>> {
     let output = std::process::Command::new("exiftool")
-            .arg("-p")
-            .arg(r#"${dateTimeOriginal#;DateFmt("%Y-%m-%d %H:%M:%S")}.${subSecTimeOriginal} ${dateTimeOriginal#;DateFmt("%z")}"#)
-            .arg(file_path)
-            .output()?;
+        .args(args)
+        .output()
+        .map_err(|error| format!("Failed executing command: {}", error))?;
 
-    let stderr = std::str::from_utf8(&output.stderr)?;
+    let error = match output.status.code() {
+        Some(code) if code == 0 => None,
+        Some(code) => Some(format!("Process has terminated with code {}", code)),
+        None => Some("Process has been terminated by signal".to_owned()),
+    };
 
-    if stderr != "" {
-        return Err(Box::new(Error {
-            message: stderr.to_owned(),
-        }));
-    }
+    let stderr = std::str::from_utf8(&output.stderr)
+        .map_err(|error| format!("Failed encoding stderr as UTF-8: {}", error))?;
 
-    let stdout = std::str::from_utf8(&output.stdout)?;
+    match (error, stderr) {
+        (None, "") => Ok(()),
+        (None, stderr) => Err(format!("Stderr: {}", stderr)),
+        (Some(error), "") => Err(error),
+        (Some(error), stderr) => Err(format!("{}. Stderr: {}", error, stderr)),
+    }?;
 
-    let mut timestamp = stdout.to_owned();
-    timestamp.pop();
+    let stdout = std::str::from_utf8(&output.stdout)
+        .map_err(|error| format!("Failed encoding stdout as UTF-8: {}", error))?;
 
-    let timestamp = DateTime::parse_from_str(&timestamp, "%Y-%m-%d %H:%M:%S%.f %z")?;
+    Ok(stdout.to_owned())
+}
+
+fn get_timestamp(file_path: &str) -> Result<[u8; 8], Box<dyn std::error::Error>> {
+    let output = exiftool(&[
+        "-p",
+        r#"${dateTimeOriginal#;DateFmt("%Y-%m-%d %H:%M:%S")}.${subSecTimeOriginal} ${dateTimeOriginal#;DateFmt("%z")}"#,
+        file_path
+    ]).map_err(|error| format!("Failed running exiftool: {}", error))?;
+
+    let timestamp = DateTime::parse_from_str(&output, "%Y-%m-%d %H:%M:%S%.f %z\n")
+        .map_err(|error| format!("Failed parsing exiftool timestamp: {}", error))?;
+
     let timestamp = timestamp.timestamp_millis();
     let timestamp = unsafe { std::mem::transmute::<_, [u8; 8]>(timestamp.to_be()) };
 
@@ -85,19 +89,20 @@ fn get_fingerprint(file_path: &str) -> Result<[u8; 32], Box<dyn std::error::Erro
     let image = rawloader::decode_file(file_path)?;
 
     let data: Vec<u8> = match image.data {
-        rawloader::RawImageData::Float(data) => unsafe { transmute_vec(data)? },
-        rawloader::RawImageData::Integer(data) => unsafe { transmute_vec(data)? },
-    };
+        rawloader::RawImageData::Float(data) => unsafe { transmute_vec(data) },
+        rawloader::RawImageData::Integer(data) => unsafe { transmute_vec(data) },
+    }
+    .map_err(|error| format!("Failed transmuting data: {}", error))?;
 
     let mut hasher = sha2::Sha256::new();
     hasher.input(data);
 
     let sha256 = hasher.result();
 
-    let mut array: [u8; 32] = Default::default();
-    array.copy_from_slice(&sha256[..32]);
+    let mut fingerprint: [u8; 32] = Default::default();
+    fingerprint.copy_from_slice(&sha256);
 
-    Ok(array)
+    Ok(fingerprint)
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -109,8 +114,11 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .value_of("file path")
         .ok_or("No file path provided")?;
 
-    let timestamp = get_timestamp(file_path)?;
-    let fingerprint = get_fingerprint(file_path)?;
+    let timestamp = get_timestamp(file_path)
+        .map_err(|error| format!("Failed generating timestamp data: {}", error))?;
+
+    let fingerprint = get_fingerprint(file_path)
+        .map_err(|error| format!("Failed generating fingerprint data: {}", error))?;
 
     println!(
         "{}-{}",
